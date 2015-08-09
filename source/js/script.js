46074b40fe7b568e103ba0498025b499876377ef
ekompanets <amelie.kom@gmail.com>
2015-08-09 14:01:37 +0300

    Almost done

--- source/js/script.js
+++ source/js/script.js
@@ -1,30 +1,51 @@
 (function(){
-  //  мобильное меню
+  //   меню
 
-  var iMenu=document.querySelector(".main-nav__item a");
-  // var navClose=document.querySelector(".main-nav__item ");
-  // var mainNav=document.querySelector(".main-nav");
+  var menuElements=document.querySelectorAll(".main-nav__item");
+  for (var i=0; i<menuElements.length; i++) {
+    OpenSubMenu(menuElements[i])
+  }
 
-  iMenu.addEventListener("mouseover", function(event) {
-      console.log('click'+iMenu);
-      event.preventDefault();
-      iMenu.classList.add("show");
-  })
+  function OpenSubMenu(iMenu) {
+    iMenu.addEventListener("mouseover", function(event) {
+        console.log('click'+iMenu);
+        event.preventDefault();
+        iMenu.classList.add("show");
+    })
 
-  iMenu.addEventListener("mouseout", function(event) {
+    iMenu.addEventListener("mouseout", function(event) {
       console.log('click'+iMenu);
       event.preventDefault();
       if (iMenu.classList.contains("show")) {
         iMenu.classList.remove("show");
       };
   })
+  }
 
-  // navClose.addEventListener("click", function(event) {
-  //   event.preventDefault();
-  //    if (mainNav.classList.contains("main-nav__show")) {
-  //     mainNav.classList.remove("main-nav__show");
-  //    }
-  // })
+
+  //  --  меню
+
+  //  мобильное меню
+
+  var toggle=document.querySelector(".toggle");
+  var navClose=document.querySelector(".toggle-close");
+  var mainNav=document.querySelector(".main-nav");
+
+  toggle.addEventListener("click", function(event) {
+      event.preventDefault();
+      mainNav.classList.add("main-nav__show");
+      toggle.classList.add("hidden");
+  })
+
+  navClose.addEventListener("click", function(event) {
+    event.preventDefault();
+     if (mainNav.classList.contains("main-nav__show")) {
+      mainNav.classList.remove("main-nav__show");
+     }
+     if (toggle.classList.contains("hidden")) {
+      toggle.classList.remove("hidden");
+     }
+  })
 
   //  -- мобильное меню
 })();
